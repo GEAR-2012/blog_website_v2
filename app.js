837e7979ce1e5f1a2988413665d553c02eae4210
GEAR-2012 <sandor.tudja@gmail.com>
2023-01-28 19:31:08 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const _ = require("lodash");
+const dotenv = require("dotenv").config();
+//
+const { homeContent, aboutContent, contactContent } = require("./contents");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.set("strictQuery", true);
+
+const dbName = "blogPostDB";
+const dbUserName = "admin-sandor";
+const dbPassword = "Test123";
+const uri = `mongodb+srv://${dbUserName}:${dbPassword}@cluster0.i8f6e4b.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+main();
+
+async function main() {
+  console.log("function 'main' started");
+  try {
+    await mongoose.connect(uri);
+    const blogPostSchema = new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+      },
+      body: {
+        type: String,
+        required: true,
+      },
+    });
+    const Blogpost = mongoose.model("Blogpost", blogPostSchema);
+
+    app.listen(3000, () => console.log("Servers was started at port 3000"));
+
+    app.get("/", async (req, res) => {
+      const blogPosts = await Blogpost.find().exec();
+      res.render("home", { pageTitle: "Home", pageContent: homeContent, blogPosts });
+    });
+
+    app.post("/", async (req, res) => {
+      const blogpost = new Blogpost({
+        title: req.body.postTitle,
+        body: req.body.postBody,
+      });
+      await blogpost.save();
+      res.redirect("/");
+    });
+
+    app.get("/update/:postId", async (req, res) => {
+      const postId = req.params.postId;
+      const filter = { _id: postId };
+      const foundBlogPost = await Blogpost.findOne(filter).exec();
+      const post = foundBlogPost;
+      res.render("update", { pageTitle: "Construct", post });
+    });
+
+    app.post("/update/:postId", async (req, res) => {
+      const postId = req.params.postId;
+      const filter = { _id: postId };
+      const updateDoc = {
+        title: req.body.postTitle,
+        body: req.body.postBody,
+      };
+      const updateResult = await Blogpost.updateOne(filter, updateDoc).exec();
+      console.log(updateResult);
+      res.redirect(`/post/${postId}`);
+    });
+
+    app.get("/post/:postId", async (req, res) => {
+      const requiredPostId = req.params.postId;
+      const filter = { _id: requiredPostId };
+      const projection = {};
+      const foundBlogPost = await Blogpost.findOne(filter, projection).exec();
+      res.render("post", foundBlogPost);
+    });
+
+    app.post("/delete/:postId", async (req, res) => {
+      const postId = req.params.postId;
+      const filter = { _id: postId };
+      const result = await Blogpost.deleteOne(filter).exec();
+      res.redirect("/");
+    });
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+app.get("/construct", async (req, res) => {
+  res.render("construct", { pageTitle: "Construct" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { pageTitle: "About Us", pageContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { pageTitle: "Contact Us", pageContent: contactContent });
+});
---
+++ contents.js
@@ -0,0 +1,12 @@
+const homeContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+const aboutContent =
+  "Just say anything, George, say what ever's natural, the first thing that comes to your mind. Take that you mutated son-of-a-bitch. My pine, why you. You space bastard, you killed a pine. You do? Yeah, it's 8:00. Hey, McFly, I thought I told you never to come in here. Well it's gonna cost you. How much money you got on you?";
+const contactContent =
+  "Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before. Many say exploration is part of our destiny, but it’s actually our duty to future generations and their quest to ensure the survival of the human species.";
+
+module.exports = {
+  homeContent,
+  aboutContent,
+  contactContent,
+};
